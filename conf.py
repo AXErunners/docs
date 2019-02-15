95a6935c272fd110aecebdf7c3bf30e08b8c7ea1
-k <slowdive@me.com>
2019-02-15 13:52:13 -0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Axe'
-copyright = u'2018, Axe Core Group, Inc'
+copyright = u'2019, AXErunners'
 author = u'strophy'
 
 # The version info for the project you're documenting, acts as replacement for
